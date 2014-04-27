c159eba8d4476cfce8f8f898e302d8b5359058d9
John Kasiewicz <crestcode@gmail.com>
2014-04-27 15:27:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QbwcMu::Application.config.secret_token = 'ac13283f0d26f7165ce7f08481891a1cdde6c2a959dcc0f9e41391d49afb7f87b36906039eee8d4f719c2dbb10bff709c198dcb4758db2a9312592bb41f90cf9'
